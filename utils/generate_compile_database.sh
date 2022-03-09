a4047242094ec42065b38c9970c60c4c9bc1c047
Andrey Savelyev <andre-savelev@yandex.ru>
2022-03-09 17:34:18 +0300

    script for compile database generation

---
+++ utils/generate_compile_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PROJECT_PATH="`dirname \"$0\"`"                    # relative path
+PROJECT_PATH="`(cd \"$PROJECT_PATH\" && pwd)`"      # absolute path
+
+if [ "$#" -eq 1 ]; then
+    PROJECT_PATH="`(cd \"$1\" && pwd)`"            # absolute path
+fi
+
+compdb -p "${PROJECT_PATH}/build" list
