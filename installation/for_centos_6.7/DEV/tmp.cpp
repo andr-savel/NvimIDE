9291d0b9b0befa72e93937b785763f5bd0a385d4
Andrey Savelyev <andre-savelev@yandex.ru>
2022-07-05 18:03:11 +0300

    final installation procedure

---
+++ installation/for_centos_6.7/DEV/tmp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void fff() {
+    int a = 10;
+    char b = 'a';
+
+    std::cout<<"a = "<< a <<"   "<<int(b)<<std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    fff();
+    fff();
+
+    fff();
+    fff();
+    fff();
+
+
+    std::cout<<"qweqwe"<<std::endl;
+    return 0;
+}
