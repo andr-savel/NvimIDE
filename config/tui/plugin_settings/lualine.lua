0f34b9834ba9554081eaf2ecbabc9f0a561c282d
Andrey Savelyev <andre-savelev@yandex.ru>
2022-08-12 15:54:57 +0300

    correct file names in statusline; correct colors in terminal mode; correct modes switch from terminal

--- config/tui/plugin_settings/lualine.lua
+++ config/tui/plugin_settings/lualine.lua
@@ -82,6 +82,14 @@ require'lualine'.setup {
         lualine_y = {'progress'},
         lualine_z = {'location'}
     },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {filename},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+    },
     tabline = {},
     extensions = {'quickfix'}
 }
