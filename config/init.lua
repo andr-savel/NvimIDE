ba38b2788b273dfac5a924ebf1835622638d5061
Andrey Savelyev <andre-savelev@yandex.ru>
2026-04-29 13:46:43 +0300

    minor

---
+++ config/init.lua
@@ -0,0 +1,13 @@
+local prefix = "source " .. os.getenv("HOME") .. "/.config/nvim/tui/"
+
+vim.cmd(prefix .. "project")
+vim.cmd(prefix .. "plugins")
+vim.cmd(prefix .. "editor")
+vim.cmd(prefix .. "file")
+if vim.g.nvim_ide_project_root ~= nil then
+    vim.cmd(prefix .. "search_in_content")
+end
+vim.cmd(prefix .. "search_map")
+vim.cmd(prefix .. "terminal")
+vim.cmd(prefix .. "win_buf")
+
